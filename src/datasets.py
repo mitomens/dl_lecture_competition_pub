d402dfe3eb4efe4a68567e3bad8544aa66e3cdd1
keitomitome <keitokf6@g.ecc.u-tokyo.ac.jp>
2024-07-17 23:01:09 +0900

    正規化の実装

--- src/datasets.py
+++ src/datasets.py
@@ -130,3 +130,38 @@ def set_lr(lr, optimizer):
     for param_group in optimizer.param_groups:
         param_group["lr"] = lr
 
+class StandardScalerSubset(torch.utils.data.Dataset):
+    def __init__(self, dataset, indices,
+                 mean=None, std=None, eps=10**-9):
+        super().__init__(dataset=dataset, indices=indices)
+        target_tensor = torch.stack([dataset[i][0] for i in indices])
+        target_tensor = target_tensor.to(torch.float64)
+        if mean is None:
+            self._mean = torch.mean(target_tensor, dim=0)
+        else:
+            self._mean = mean
+        if std is None:
+            self._std = torch.std(target_tensor, dim=0, unbiased=False)
+        else:
+            self._std = std
+        self._eps = eps
+        self.std.apply_(lambda x: max(x, self.eps)) # ゼロ割対策
+
+    def __getitem__(self, idx):
+        dataset_list = list(self.dataset[self.indices[idx]])
+        input = dataset_list[0]
+        dataset_list[0] = (input - self.mean) / self.std
+        return tuple(dataset_list)
+
+    @property
+    def mean(self):
+        return self._mean
+
+    @property
+    def std(self):
+        return self._std
+
+    @property
+    def eps(self):
+        return self._eps
+
